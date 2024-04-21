062fcd84076ab28926819941ad62bdee4facaff4
Isaac Granados Gomez <isaacxdd@gmail.com>
2024-04-20 23:20:03 -0600

    Carrito2

--- TiendaProyecto/Controllers/HomeController.cs
+++ TiendaProyecto/Controllers/HomeController.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using TiendaProyecto.Models;
 using Microsoft.EntityFrameworkCore;
-using TiendaProyecto.Migrations;
 
 
 namespace TiendaProyecto.Controllers
